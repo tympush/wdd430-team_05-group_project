d164c83bbba6c7b0d5495a33418b77adbb708628
steven_savarin <srssdesing@gmail.com>
2025-11-13 15:35:05 +0100

    components to app

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-import Navbar from "@/components/Navbar";
-import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
